cd6f0723d887af1ff15ed7f4c875d6ab6955ad44
Jose Berrocal <jose.berrocal@alkaid.cr>
2017-11-03 09:51:32 -0600

    Initial and probably only commit

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+
+createIndexFile(process.cwd());
+
+/**
+ * creates a index file per each folder
+ * @param {String} filePath
+ */
+function createIndexFile(filePath) {
+    const files = fs.readdirSync(filePath);
+    files.forEach(fileOrDirectory => {
+        if (fs.lstatSync(path.join(filePath, fileOrDirectory)).isDirectory()) {
+            createIndexFile(path.join(filePath, fileOrDirectory));
+        }
+    });
+    if (!fs.existsSync(path.join(filePath, 'index.ts')))
+        fs.writeFileSync(path.join(filePath, 'index.ts'), files.filter(file => file.endsWith('.ts')).map(file => `export * from './${file.replace(/\.ts$/, '')}';`).join('\n'));
+}
